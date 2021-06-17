93c60fa4a2514c860bf67523f8596e3ca241c3f6
vitalispopoff <vitalis.popoff@gmail.com>
2021-06-17 22:00:11 +0200

    ch19 - further descr of vector

--- ch19/ch19.h
+++ ch19/ch19.h
@@ -8,5 +8,178 @@ namespace ch19
 		{
 			int main();
 		}
+
+		namespace s02
+		{
+			int main();
+		}
+
+		namespace s03
+		{
+			class m_vector
+			{
+				int
+					sz,
+					space;
+				double
+					* elem;
+			public :
+				m_vector() :
+					sz {0},
+					elem {nullptr},
+					space {0}		///not very well defined, isn't it?
+				{}
+				void reserve (int newalloc);
+				int capacity() const
+				{
+					return space;
+				}
+			};
+		}
+
+		namespace s04
+		{
+			class m_vector
+			{
+				int
+					sz,
+					space;
+				double
+					* elem;
+				///...
+				void reserve (int newalloc) 
+				{
+					/// vide s03::m_vector::reserve()
+				}
+				void resize(int newsize);
+			};
+		}
+
+		namespace s05
+		{
+			class m_vector
+			{
+				int
+					sz,
+					space;
+				double
+					* elem;
+				///...
+				void reserve (int newalloc)
+				{
+					/// vide s03::m_vector::reserve()
+				}
+				void resize (int newsize)
+				{
+					/// vide s04::m_vector::resize()
+				}
+				void push_back (double d);
+			};
+		}
+
+		namespace s06
+		{
+			class m_vector
+			{
+				int
+					sz,
+					space;
+				double
+					* elem;
+				///...
+				void reserve (int newalloc)
+				{
+					/// vide s03::m_vector::reserve()
+				}
+				void resize (int newsize)
+				{
+					/// vide s04::m_vector::resize()
+				}
+				void push_back (double d)
+				{
+					/// vide s05::m_vector::push_back()
+				}
+				m_vector & operator = (const m_vector & a);
+
+			};
+		}
+
+		namespace s07
+		{
+			class m_vector
+			{
+				int
+					sz,
+					space;
+				double
+					* elem;
+				///...
+				void reserve (int newalloc)
+				{
+					/// vide s03::m_vector::reserve()
+				}
+				void resize (int newsize)
+				{
+					/// vide s04::m_vector::resize()
+				}
+				void push_back (double d)
+				{
+					/// vide s05::m_vector::push_back()
+				}
+				m_vector & operator = (const m_vector & a);
+			};			
+		}
+
+		namespace s08
+		{
+			class m_vector
+			{
+				int
+					sz,
+					space;
+				double
+					* elem;
+			public:
+				m_vector () :
+					sz {0},
+					elem {nullptr},
+					space {0}
+				{}
+				explicit m_vector (int s) :
+					sz {s},
+					elem {new double[s]},
+					space {s}
+				{
+					for (int i = 0; i < sz; ++i)
+						elem [i] = 0;
+				}
+				m_vector (const m_vector &);
+				m_vector & operator = (const m_vector &);
+				m_vector & operator = (m_vector &&);
+				~m_vector() 
+				{
+					delete [] elem;
+				}
+				double & operator [] (int n)
+				{
+					return elem [n];
+				}
+				const double & operator [] (int n) const
+				{
+					return elem [n];
+				}
+				int size () const
+				{
+					return sz;
+				}
+				int capacity () const 
+				{
+					return space;
+				}
+				void resize (int new_size);
+				void puch_back (double d);
+				void reserve (int new_alloc);
+			};
+		}
 	}
 }
--- ch19/ch19_sample.cpp
+++ ch19/ch19_sample.cpp
@@ -1,16 +1,136 @@
 #include "ch19.h"
-
+#include <vector>
+#include <iostream>
 namespace ch19
 {
 	namespace sample
 	{
+		using namespace std;
 		namespace s01
 		{
-			int main();
+			int main()
 			{
+				vector <double>
+					* p = new vector <double> (10);
+				int
+					n = 0;
+				for (double d; cin >> d; )
+				{
+					if (n == p -> size())
+					{
+						vector <double> 
+							* q = new vector <double> (p -> size() * 2);
+						copy (p -> begin(), p -> end(), q -> begin());
+						delete p;
+						p = q;
+					}
+					(* p) [n] = d;
+					++n;
+				}
 				return 0;
 			}
 		}
+
+		namespace s02
+		{
+			int main()
+			{
+				vector <double>
+					v,
+					v2(1);
+				v.resize(10);
+				v.push_back (.7);
+				v = v2;
+
+				return 0;
+			}
+		}
+
+		namespace s03
+		{
+			void m_vector::reserve (int newalloc)
+			{
+				if (newalloc <= space)
+					return;
+				double
+					* p = new double [newalloc];
+				for (int i = 0; i < sz; ++i)
+					p[i] = elem [i];
+				delete [] elem;
+				elem = p;
+				space = newalloc;
+			}
+		}
+
+		namespace s04
+		{
+			void m_vector::resize(int newsize)
+			{				
+				reserve (newsize);				
+				for (int i = sz; i < newsize; ++i)	
+					elem[i] = 0;
+				sz = newsize;
+			}
+		}
+
+		namespace s05
+		{
+			void m_vector::push_back (double d)
+			{
+				if (space == 0)
+					reserve (8);
+				else
+					if (sz == space)
+						reserve (space * 2);
+				elem [sz] = d;
+				++sz;
+			}
+		}
+
+		namespace s06
+		{
+			m_vector & m_vector::operator = (const m_vector & a)
+			{
+				double 
+					* p = new double[a.sz];
+				for (int i = 0; i < a.sz; ++i)
+					p[i] = a.elem[i];
+				delete [] elem;
+				space = sz = a.sz;
+				elem = p;
+				return * this;
+			}
+		}
+
+		namespace s07
+		{
+			m_vector & m_vector::operator = (const m_vector & a)
+			{
+				if (this == &a)
+					return * this;
+				if (a.sz <= space)
+				{
+					for (int i = 0; i < a.sz; ++i)
+						elem [i] = a.elem[i];
+					sz = a.sz;
+					return * this;
+				}
+				double
+					* p = new double [a.sz];
+				for (int i = 0; i < a.sz; ++i)
+					p[i] = a.elem[i];
+				delete [] elem;
+				space = sz - a.sz;
+				elem = p;
+				return * this;
+			}
+		}
+
+		namespace s08
+		{
+			void m_vector::resize (int new_size){}
+			void m_vector::puch_back (double d){}
+			void m_vector::reserve (int new_alloc){}
+		}
 	}
 }
-
